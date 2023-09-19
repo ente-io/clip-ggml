eb245a15a438a39c8c73b8f47a0f22a52c453e4b
0nullpointer <nullpointer@skiff.com>
2023-09-19 17:12:30 +0200

    Change dart api

--- lib/clip_ggml.dart
+++ lib/clip_ggml.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 import 'package:ffi/ffi.dart';
 
 typedef clip_ggml_request_native = ffi.Pointer<Utf8> Function(
-    ffi.Pointer<Utf8> input);
+    ffi.Pointer<Utf8> dart_model_path,
+    ffi.Pointer<Utf8> dart_image_path,
+    ffi.Pointer<Utf8> dart_text);
 
 class Clip_GGML {
   String clip_lib = "libclip_ggml.so";
@@ -26,12 +28,19 @@ class Clip_GGML {
     }
   }
 
-  String native_request({required String inputString, String? clipLib}) {
+  String native_request(
+      {required String modelString,
+      required String imageString,
+      required String textString,
+      String? clipLib}) {
     clipLib ??= clip_lib;
-    ffi.Pointer<Utf8> input_string = inputString.toNativeUtf8();
+    ffi.Pointer<Utf8> model_string = modelString.toNativeUtf8();
+    ffi.Pointer<Utf8> image_string = imageString.toNativeUtf8();
+    ffi.Pointer<Utf8> text_string = textString.toNativeUtf8();
     var res = openLib(clipLib: clipLib)
-        .lookupFunction<clip_ggml_request_native, clip_ggml_request_native>("test")
-        .call(input_string);
+        .lookupFunction<clip_ggml_request_native, clip_ggml_request_native>(
+            "test")
+        .call(model_string, image_string, text_string);
     //Might need to edit this since this a Utf8 pointer
     return res.toDartString();
   }
