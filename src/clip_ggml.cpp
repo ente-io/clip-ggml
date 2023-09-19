8fc58c2568dc44f088f0699e0a04beced55c712b
vishnukvmd <vishnu@ente.io>
2023-09-19 20:04:05 +0530

    Update the model path

--- src/clip_ggml.cpp
+++ src/clip_ggml.cpp
@@ -2,8 +2,6 @@
 #include "clip.cpp/clip.h"
 #include <vector>
 
-
-std::string model = "models/openai_clip-vit-base-patch32.ggmlv0.f16.bin";
 std::string image_path;
 std::string text;
 
@@ -19,16 +17,11 @@ extern "C"
     ggml_time_init();
     const int64_t t_main_start_us = ggml_time_us();
     char *result = str_to_charp(std::to_string(t_main_start_us));
-    
-    auto ctx = clip_model_load(model.c_str(), 1);
+    auto ctx = clip_model_load(input, 1);
     if (!ctx){
       std::string error_message = "Model not loaded";
       return str_to_charp(error_message);
     }
     return result;
   }
-
-  
-
-
 }
