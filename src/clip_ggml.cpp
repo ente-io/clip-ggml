e7daff4d0a424e9733829b28a4aa4baab65bae6a
0nullpointer <nullpointer@skiff.com>
2023-09-19 11:07:56 +0200

    Add model loading test

--- src/clip_ggml.cpp
+++ src/clip_ggml.cpp
@@ -1,20 +1,34 @@
 #include <string>
 #include "clip.cpp/clip.h"
+#include <vector>
+
+
+std::string model = "models/openai_clip-vit-base-patch32.ggmlv0.f16.bin";
+std::string image_path;
+std::string text;
+
+char *str_to_charp(std::string s) {
+  char *result = new char[s.size() + 1];
+  strcpy(result, s.c_str());
+  return result;
+}
 
 extern "C"
 {
   char * test(char * input) {
-    // std::string sample = "test works";
-    // char *result = new char[sample.size() + 1];
-    // strcpy(result, sample.c_str());
-    // return result;
     ggml_time_init();
     const int64_t t_main_start_us = ggml_time_us();
-    std::string time = std::to_string(t_main_start_us);
-    char *result = new char[time.size() + 1];
-    strcpy(result, time.c_str());
+    char *result = str_to_charp(std::to_string(t_main_start_us));
+    
+    auto ctx = clip_model_load(model.c_str(), 1);
+    if (!ctx){
+      std::string error_message = "Model not loaded";
+      return str_to_charp(error_message);
+    }
     return result;
   }
+
+  
 
 
 }
