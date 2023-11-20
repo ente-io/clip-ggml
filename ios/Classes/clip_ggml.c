5fb40bc7d4a48850545737fb34578a1753487a0d
0nullpointer <nullpointer@skiff.com>
2023-11-20 16:32:42 +0100

    Add comment to ios classes

--- ios/Classes/clip_ggml.c
+++ ios/Classes/clip_ggml.c
@@ -1,3 +1,5 @@
 // Relative import to be able to reuse the C sources.
 // See the comment in ../{projectName}}.podspec for more information.
+
+// Will have to change this for clip_ggml.cpp
 #include "../../src/clip_ggml.c"
